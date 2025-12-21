494e20d2e8c155b8558d449b64abe759d141cc9f
Stine Knutsen <160833015+stineknutsen@users.noreply.github.com>
2025-12-21 19:33:31 +0100

    add contact links

--- portfolio/src/pages/Contact/index.jsx
+++ portfolio/src/pages/Contact/index.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 export default function Contact() {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText("stineknutsen02@gmail.com").then(() => {
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    });
+  };
+
   return (
     <section className="bg-primary/30 dark:bg-primary/90 py-8 min-h-screen">
       <div className="bg-background text-primary dark:bg-primary dark:text-background flex flex-col h-[100%] w-[90%] max-w-[1200px] mx-auto p-6 rounded-md shadow-lg">
@@ -10,14 +23,28 @@ export default function Contact() {
         </p>
 
         <div className="flex gap-4 mx-auto">
-          <a href="#" className="">
-            <i class="fa-solid fa-envelope text-4xl"></i>
+          <div>
+            <button onClick={handleCopy} title="Copy email to clipboard">
+              <i class="fa-solid fa-envelope text-4xl hover:scale-110 transition duration-300"></i>
+            </button>
+            {copied && <p className="text-sm">Copied to clipboard!</p>}
+          </div>
+
+          <a
+            href="https://www.linkedin.com/in/stine-knutsen-8b9557209/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Stinke Knutsen on Linkedin"
+          >
+            <i class="fa-brands fa-linkedin text-4xl hover:scale-110 transition duration-300"></i>
           </a>
-          <a href="#" target="_blank" className="">
-            <i class="fa-brands fa-linkedin text-4xl"></i>
-          </a>
-          <a href="#" target="_blank" className="">
-            <i class="fa-brands fa-github text-4xl"></i>
+          <a
+            href="https://github.com/stineknutsen"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Stine Knutsen on Github"
+          >
+            <i class="fa-brands fa-github text-4xl hover:scale-110 transition duration-300"></i>
           </a>
         </div>
       </div>
